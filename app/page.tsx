314701b0ee7f05af0119a76fbc2327f88fa85360
Arun Ramamurthy <arun_r80@yahoo.co.in>
2025-10-26 06:40:25 +1100

    Added ReadMe file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
-import HeaderBar from "@/app/ui/containers";
+import HeaderBar, { SideBar } from "@/app/ui/containers";
 
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-4">
       <HeaderBar />
+      <div className="flex flex-col grow mt-2 w-full min-h-full">
+        <SideBar />
+      </div>
     </main>
   );
 }
--- app/ui/containers.tsx
+++ app/ui/containers.tsx
@@ -2,6 +2,9 @@
 Components to render container components of the app
 */
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+type emptyProps = {};
+
 export default function HeaderBar() {
   return (
     <div className="flex flex-col justify-center bg-blue-500 rounded-lg h-24">
@@ -12,6 +15,35 @@ export default function HeaderBar() {
   );
 }
 
-export function ChatContainer() {
+export function ChatContainer({}: emptyProps) {
   return;
+}
+
+export function SideBar() {
+  return (
+    <div className="flex flex-col justify-center rounded-lg grow w-2/9 p-2 text-center bg-blue-500 min-h-full">
+      FormAssisstant - Pre-configured, Customizable and Hallucinophilic
+    </div>
+  );
+}
+
+function ChatMessages({
+  message,
+  userType,
+}: {
+  message: string;
+  userType: string;
+}): JSX.Element {
+  return (
+    <div
+      className={
+        "flex flex-row rounded-lg " +
+        (userType == "user"
+          ? "justify-right bg-blue-500 text-white"
+          : "justify-left text-blue-500")
+      }
+    >
+      <p className="p-2">{message}</p>
+    </div>
+  );
 }
